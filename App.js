a9d865fe36eed1ebce98bb340b21e1e03dc8a250
Your Name <you@example.com>
2024-06-26 08:21:26 +0530

    develop react application

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 let heading = React.createElement("h1", {id:"heading", onClick:headingFunc}, "Hello world from React");
 let root = ReactDOM.createRoot(document.getElementById("root"));
 // all react code runs only in root .
@@ -12,9 +16,10 @@ function headingFunc(){
 
 
 let child = React.createElement("div",{id:"child"}, heading);
-let parent = React.createElement("div",{id:"parent"}, [child, child]);
+let parent = React.createElement("div",{id:"parent"}, [child]);
 
 root.render(parent);
 
-// React is library because it can work in a small portion of our app as well where root are.
+// React is library because it can work in a small portion of our app as well as where root are.
 // framework : it provides a complete structure to build our app
+
